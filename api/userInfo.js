eb5aa1c7b9738afcc56fd4294616e611c1b9c881
zsg179 <18977330642@163.com>
2021-06-04 17:34:52 +0800

    增加用户实名认证页面

--- api/userInfo.js
+++ api/userInfo.js
@@ -1,13 +1,27 @@
-import request from '@/utils/request'
+import request from '@/utils/request'
 
-const api_name = `/api/user`
+const api_name = `/api/user`
 
-export default {
-    login(userInfo) {
-        return request({
-            url: `${api_name}/login`,
-            method: `post`,
-            data: userInfo
-        })
-    }
+export default {
+    login(userInfo) {
+        return request({
+            url: `${api_name}/login`,
+            method: `post`,
+            data: userInfo
+        })
+    },
+    getUserInfo() {
+        return request({
+            url: `${api_name}/auth/getUserInfo`,
+            method: `get`
+        })
+    },
+
+    saveUserAuah(userAuah) {
+        return request({
+            url: `${api_name}/auth/userAuah`,
+            method: 'post',
+            data: userAuah
+        })
+    }
 }
